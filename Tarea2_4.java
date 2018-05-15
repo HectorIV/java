69fd8c5611a6c3f210d83da1761bd4bf3272208f
Héctor <hectori96@hotmail.es>
2018-05-14 23:52:01 -0600

    Correcion de la tarea

--- Tarea2_4.java
+++ Tarea2_4.java
@@ -6,11 +6,11 @@
  * Rudas Felix 8-928-508
  */
 import java.io.*;
+import java.util.regex.*;
 
 class AsignarArchivos
 {
   private String cont;
-  private String digi, letra;
   
   private FileReader    f;
   private BufferedReader    e;
@@ -21,7 +21,8 @@ class AsignarArchivos
   //constructor de la clase Crea_archivo
   AsignarArchivos () throws Exception
   {  try 
-       { f = new FileReader ("carácter.txt");
+       { 
+       	 f = new FileReader ("carácter.txt");
          e = new BufferedReader  (f) ;  
          	
          fd = new FileWriter ("dígitos.txt"); 
@@ -34,52 +35,66 @@ class AsignarArchivos
        { throw y; }  
   }
    
-    public static boolean isNumeric(String cadena) 
+//Este metodo usa las expresiones regulares para encontrar cooncidencias entre una cade y el patron que se desea 
+     public static int expRegular(String cadena) 
      {
-
-        boolean resultado;
-      
-        try {
-            Integer.parseInt(cadena);	
-            resultado = true;
-        } catch (NumberFormatException excepcion) {
-            resultado = false;
+     	int resultado;
+     	//Se declara el objeto p que tendra el patron que en este caso es para buscar si son letras y el objeto m evalua la cadena con el patron
+        Pattern p = Pattern.compile("[a-zA-Z]");
+        Matcher m = p.matcher(cadena);
+        //Se declara el objeto p que tendra el patron que en este caso es para buscar si son numeros y el objeto m evalua la cadena con el patron
+        Pattern p1 = Pattern.compile("[0-9]");
+        Matcher m1 = p1.matcher(cadena);
+        //find verifica si el objeto m se encuentra una expresión regular
+        if(m.find()==true)
+        {
+        	resultado = 1;
         }
-        
+        //find verifica si el objeto m1 se encuentra una expresión regular
+        else if(m1.find()==true)
+        {
+        	resultado = 2;
+        }
+        //En caso de no encontrar ninguna le asigna 3 para que no grabe caracteres especiales
+        else{ 
+        	
+        	resultado = 3;
+        }
         return resultado;
-    }   
-   boolean leer ()  throws Exception
+    }   	
+    	
+   boolean leerEscribir ()  throws Exception
    {   boolean fin = false;
-       
+       String digi, letra;
        try 
          {  
            	cont=e.readLine ();
           // para controlar fin de archivo ( no hay más datos)
-          // se verifica si el registro es null
+          // se verifica si el registro es null 
           if (cont == null)
-           { 
+           { //Si fin es true en el main rompe el while y se cierra el programa
            	  fin = true;
            
            }
-           //llama a la funcion isNumeric verifica si es un numero entero o un caracter
-           else if(isNumeric(cont) == true)
+           //llama a la funcion expRegular verifica si es un numero entero o una letra
+           else if(expRegular(cont) == 1)
+            {
+              l.println(cont);
+            }
+            	
+            else if(expRegular(cont) == 2)
             {
               d.println(cont);
             }
-            	
-            else
-            {
-              l.println(cont);
-            }	
-            	
+            
+            
           }                  
          catch (IOException y)
           {throw y; }
+   //Retorna fin para cerrar el ciclo en el main y no se repita
      return fin;         
    }
     
-   String mostrarC()
-   { return cont;}
    
    void cerrar() throws Exception
    { try 
@@ -102,24 +117,26 @@ class Tarea2_4
 {
 	public static void main (String arg[] ) throws Exception 
   { 
-
+ //Se crea la variable fin
   boolean fin;
+  
   try
-  { AsignarArchivos obj = new AsignarArchivos();
+  {   AsignarArchivos obj = new AsignarArchivos();
       System.out.println ("El contenido del documento carácter.txt es: ");
-    fin = obj.leer();     
+    //Se inicializa la variable fin en false
+    fin = obj.leerEscribir();     
+    //El while es necesario ya que si se quita solo hace la opereración una sola vez
     while (fin == false) 
-    { try{
-    
-       System.out.println (obj.mostrarC());
-   
-   
-       fin = obj.leer();}
+    { 
+    	
+      try{
+       //Mientras fin no retorne true del metodo leerEscribir seguira leyendo caracter.txt y escribiendo digitos.txt y letra.txt
+       fin = obj.leerEscribir();}
        catch (Exception z) { 
-       	 System.out.println ("+++Error..."+z);
+       	 System.out.println ("Error..."+z);
        }     
          
-     } 
+     }
      System.out.println ("Se han separado en dos documentos uno contiene números y el otro letras, revise su carpeta. :D");
      obj.cerrar();
     }
